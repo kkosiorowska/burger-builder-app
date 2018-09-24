372adafddeb389936338bbd3e3aca0a7b302b668
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2018-09-24 15:26:13 +0200

    Fixed bugs

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient';
 
-class burgerIngredient extends Component {
+class BurgerIngredient extends Component {
     render () {
         let ingredient = null;
 
