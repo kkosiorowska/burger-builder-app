01b91ade7ab851c4fed73d901dd2bcdfc9ddbed5
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2018-10-25 19:48:49 +0200

    Order Submission & Passing Data between Pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,34 +90,12 @@ class BurgerBuilder extends Component {
     }
     purchaseContinueHandler = () => {
         // //alert('You continue!');
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     custumer: {
-        //         name: 'kkosiorowska',
-        //         address: {
-        //             street: 'Teststreet 1',
-        //             zipCode: '41342',
-        //             country: 'Poland'
-        //         },
-        //         email: 'test@test.com'
-        //     },
-        //     deliveryMethos: 'fastest'
-
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({ loading: false, purchasing: false});
-        //     })
-        //     .catch(error => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     });
 
         const queryParams =[];
         for(let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent (this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
 
         const queryString = queryParams.join('&');
         this.props.history.push({
