7396ae93b66948c6f0ccc4c157a838c8a44fe635
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-02-13 19:25:21 +0100

    Adding a Logout Link

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -9,7 +9,7 @@ const navigationItems = (props) => (
         <NavigationItem link="/orders">Orders</NavigationItem>
         { !props.isAuthenticated
             ? <NavigationItem link="/auth">Authenticate</NavigationItem>
-            : <NavigationItem link="/auth">Logout</NavigationItem> }
+            : <NavigationItem link="/logout">Logout</NavigationItem> }
     </ul>
 );
 
