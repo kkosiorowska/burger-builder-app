8ac20fd211503a6662c387fe7bc1f7c4d79c9471
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-02-13 17:34:53 +0100

    Adding Sign-In

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,8 +30,11 @@ export const auth = (email, password) => {
             password: password.value,
             returnSecureToken: true
         };
-        console.log(authData)
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBdP4nxiB8gugDD3yUvY2kKJ_3Ha6uwMz4', authData)
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBdP4nxiB8gugDD3yUvY2kKJ_3Ha6uwMz4';
+       if(!isSignup) {
+           url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBdP4nxiB8gugDD3yUvY2kKJ_3Ha6uwMz4';
+       }
+        axios.post(url, authData)
             .then( response => {
                 console.log(response);
                 dispatch(authSucces(response.data));
