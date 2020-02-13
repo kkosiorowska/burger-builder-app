8fab5d6268aa01895c6d3e227a7fb1de07f0f133
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-02-13 14:12:15 +0100

    Adding an Auth Form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -124,6 +124,7 @@ class ContactData extends Component {
         }
         return isValid;
     }
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm ={
             ...this.state.orderForm
